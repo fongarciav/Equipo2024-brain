dd408c43e6d1befcd7ee8fd6280228794c375bb1
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2025-12-10 21:08:41 +0000

    mas radio de giro

--- brain/sign_vision/strategies/intersection_strategy.py
+++ brain/sign_vision/strategies/intersection_strategy.py
@@ -65,7 +65,7 @@ class EnterIntersectionStrategy(SignStrategy):
             
             # SERVO Constants from angle_converter.py: CENTER=105, RIGHT=50, LEFT=160
             FORWARD_ANGLE = 105
-            LEFT_ANGLE = 130 
+            LEFT_ANGLE = 140 
             SPEED = 230
             ZERO_SPEED = 0
 
